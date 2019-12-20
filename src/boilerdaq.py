c3a10396fbb22a4a4b312c22d3c8d7cc46a39165
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2019-12-20 15:50:28 -0800

    migrated loops to backend

--- src/boilerdaq.py
+++ src/boilerdaq.py
@@ -128,3 +128,119 @@ def csv_write_results(results_path, fieldnames, time_read, results):
     with open(results_path, "a", newline="") as csv_file:
         csv_writer = DictWriter(csv_file, fieldnames=fieldnames)
         csv_writer.writerow(to_write)
+
+
+def daq_loop(
+    do_plot,
+    sensors,
+    delay,
+    results_raw_path,
+    raw_fieldnames,
+    flux_params,
+    results_cal_path,
+    cal_fieldnames,
+    plot_cache,
+):
+    while do_plot:
+        time_read, readings = read_sensors(sensors, delay)
+        csv_write_results(
+            results_raw_path, raw_fieldnames, time_read, readings
+        )
+
+        rand = readings[0].value
+
+        readings = calibrate_readings(readings)
+        fluxes = get_fluxes(readings, flux_params)
+        csv_write_results(
+            results_cal_path,
+            cal_fieldnames,
+            time_read,
+            readings + fluxes,
+        )
+
+        plot_cache.append(rand)
+
+
+def plot_loop():
+    global curve, plot_cache
+
+    curve.setData(plot_cache)
+
+
+def main():
+    global curve, plot_cache
+
+    sensors_path = "config/sensors.csv"
+    flux_params_path = "config/flux_params.csv"
+    results_raw_path = "results/results_raw.csv"
+    results_cal_path = "results/results_cal.csv"
+    plot_cache_length = 100
+    delay = 0.25
+
+    # loop setup
+    sensors = csv_read_sensors(sensors_path)
+    flux_params = csv_read_flux_params(flux_params_path, sensors)
+
+    time_read, readings = read_sensors(sensors, delay)
+    results_raw_path, raw_fieldnames = csv_create_results(
+        results_raw_path, time_read, readings
+    )
+
+    readings = calibrate_readings(readings)
+    fluxes = get_fluxes(readings, flux_params)
+    results_cal_path, cal_fieldnames = csv_create_results(
+        results_cal_path, time_read, readings + fluxes
+    )
+
+    values = [reading.value for reading in readings]
+    pressure = values[0]
+
+    do_plot = True
+    plot_cache = deque([pressure], maxlen=plot_cache_length)
+
+    # daq loop start in background
+    daq_thread = Thread(
+        target=daq_loop,
+        args=(
+            do_plot,
+            sensors,
+            delay,
+            results_raw_path,
+            raw_fieldnames,
+            flux_params,
+            results_cal_path,
+            cal_fieldnames,
+            plot_cache,
+        ),
+    )
+    daq_thread.daemon = True
+    daq_thread.start()
+
+    # plot loop setup
+    win = pyqtgraph.GraphicsWindow()
+    win.setWindowTitle("")
+    pyqtgraph.setConfigOption("foreground", "w")
+
+    plot = win.addPlot(title="")
+    line = pyqtgraph.mkPen((0, 255, 0), width=1)
+    plot.addLegend(offset=(10, 5))
+
+    curve = plot.plot(plot_cache, pen=line, name="",)
+
+    plot.setRange(yRange=[0, 1])
+    plot.setLabel(
+        "bottom", text="",
+    )
+    plot.showGrid(x=True, y=False)
+
+    # plot loop start
+    plot_thread = pyqtgraph.QtCore.QTimer()
+    plot_thread.timeout.connect(plot_loop)
+    plot_thread.start()
+
+    pyqtgraph.Qt.QtGui.QApplication.instance().exec_()
+    do_plot = False
+
+
+if __name__ == "__main__":
+    main()

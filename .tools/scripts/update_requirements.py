f2643c3f79eb03ae93f94d88f06c19a35b1c663c
blakeNaccarato <blake.naccarato@gmail.com>
2023-08-16 16:30:07 -0700

    Correct the script

--- .tools/scripts/update_requirements.py
+++ .tools/scripts/update_requirements.py
@@ -10,58 +10,45 @@ from dulwich.repo import Repo
 
 
 def main():
-    # Regexes to bump pandas-stubs in line with pandas
+    requirements_files = [
+        Path("pyproject.toml"),
+        *sorted(Path(".tools/requirements").glob("requirements*.txt")),
+    ]
+    with closing(repo := Repo(str(Path.cwd()))):
+        submodules = [Submodule(*item) for item in list(submodule_list(repo))]
     dependency_relation = r"(?P<relation>[=~>]=)"  # ==, ~=, or >=
-    pandas_re, pandas_stubs_re, pandas_repl = (
-        compile_mx(
+    for file in requirements_files:
+        original_content = content = file.read_text("utf-8")
+        if pandas_match := compile_specific(
             rf"""
             pandas                  # pandas
             (\[[\w,]+\])?           # e.g. [hdf5,performance] (optional)
             {dependency_relation}   # e.g. ==
             (?P<version>[\w\d\.]*)  # e.g. 2.0.2
             """
-        ),
-        compile_mx(
-            rf"""
-            (?P<dep>pandas-stubs)   # pandas-stubs
-            {dependency_relation}   # e.g. ~=
-            (?P<version>[\w\d\.]*)  # e.g. 2.0.2
-            """
-        ),
-        lambda version: rf"\g<prefix>\g<dep>\g<relation>{version}\g<suffix>",
-    )
-
-    # Regexes to bump packages which are pinned to commit hashes
-    submodule_re, submodule_repl = (
-        compile_mx(
-            r"""
-            (?P<name>\w+)@                         # name@
-            (?P<domain>git\+https://github\.com/)  # git+https://github.com/
-            (?P<org>\w+/)                          # org/
-            (?P=name)@                             # name@
-            (?P<commit>\w+)                        # <commit-hash>
-            $"""
-        ),
-        lambda commit: (
-            rf"\g<prefix>\g<name>@\g<domain>\g<org>\g<name>@{commit}\g<suffix>"
-        ),
-    )
-
-    with closing(repo := Repo(str(Path.cwd()))):
-        submodules = [Submodule(*item) for item in list(submodule_list(repo))]
-    requirements_files = [
-        Path("pyproject.toml"),
-        *sorted(Path(".tools/requirements").glob("requirements*.txt")),
-    ]
-    for file in requirements_files:
-        content = original_content = file.read_text("utf-8")
-        if pandas_match := pandas_re.search(content):
-            content = pandas_stubs_re.sub(
-                repl=pandas_repl(pandas_match["version"]), string=content
+        ).search(content):
+            content = compile_specific(
+                rf"""
+                (?P<dep>pandas-stubs)   # pandas-stubs
+                {dependency_relation}   # e.g. ~=
+                (?P<version>[\w\d\.]*)  # e.g. 2.0.2
+                """
+            ).sub(
+                repl=rf"\g<prefix>\g<dep>\g<relation>{pandas_match['version']}\g<suffix>",
+                string=content,
             )
-        for submodule in submodules:
-            content = submodule_re.sub(
-                repl=submodule_repl(submodule.commit), string=content
+        for sub in submodules:
+            content = compile_specific(
+                rf"""
+                {sub.name}@                      # name@
+                (?P<domain>git\+https://github\.com/)  # git+https://github.com/
+                (?P<org>\w+/)                          # org/
+                {sub.name}@                      # name@
+                (?P<commit>\w+)                        # <commit-hash>
+                $"""
+            ).sub(
+                repl=rf"\g<prefix>{sub.name}@\g<domain>\g<org>{sub.name}@{sub.commit}\g<suffix>",
+                string=content,
             )
         if content != original_content:
             file.write_text(encoding="utf-8", data=content)
@@ -83,8 +70,8 @@ class Submodule:
             self.name = self.name.decode("utf-8")
 
 
-def compile_mx(pattern: str) -> Pattern[str]:
-    """Compile a verbose, multi-line regular expression pattern."""
+def compile_specific(pattern: str) -> Pattern[str]:
+    """Compile verbose, multi-line regex pattern with a specific prefix and suffix."""
     return compile(
         flags=VERBOSE | MULTILINE,
         pattern=rf"""^

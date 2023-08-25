e6a36ad9ec34ac563703dcc073f7cb0587885b4f
blakeNaccarato <blake.naccarato@gmail.com>
2023-08-25 09:05:59 -0700

    Migrate some boilerplate to boilercore

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,44 +4,19 @@ from pathlib import Path
 from typing import Any
 
 import pytest
+from boilercore.testing import get_module_rel, walk_modules
 
-SRC = Path("src")
-EXAMPLES_DIR = SRC / "boilerdaq" / "examples"
+BOILERDAQ = Path("src") / "boilerdaq"
 
 EXAMPLES: list[Any] = []
-for directory in [
-    EXAMPLES_DIR,
-    *[
-        path
-        for path in EXAMPLES_DIR.iterdir()
-        if path.is_dir() and "__" not in str(path.relative_to(SRC))
-    ],
-]:
-    for example in sorted(directory.glob("[!__]*.py")):
-        module = str(example.relative_to(SRC).with_suffix("")).replace("\\", ".")
-        if module in {"boilerdaq.examples.controlled.set_voltage"}:
-            marks = [pytest.mark.skip]
-        elif module in {"boilerdaq.examples.controlled.flux_control"}:
-            marks = [pytest.mark.xfail]
-        else:
-            marks = []
-        EXAMPLES.append(pytest.param(module, marks=marks))
+for module in walk_modules(BOILERDAQ / "examples", BOILERDAQ):
+    rel_to_controlled = get_module_rel(module, "controlled")
+    if rel_to_controlled in {"set_voltage"}:
+        marks = [pytest.mark.skip]
+    elif rel_to_controlled in {"flux_control"}:
+        marks = [pytest.mark.xfail]
+    else:
+        marks = []
+    EXAMPLES.append(pytest.param(module, marks=marks))
 
-EXPERIMENTS_DIR = SRC / "boilerdaq" / "experiments"
-EXPERIMENTS: list[str] = []
-for directory in [
-    EXPERIMENTS_DIR,
-    *[
-        path
-        for path in sorted(EXPERIMENTS_DIR.iterdir())
-        if path.is_dir() and "__" not in str(path.relative_to(SRC))
-    ],
-]:
-    EXPERIMENTS.extend(
-        [
-            str(stage.relative_to(SRC).with_suffix(""))
-            .replace("\\", ".")
-            .replace("/", ".")
-            for stage in sorted(directory.glob("[!__]*.py"))
-        ]
-    )
+EXPERIMENTS = list(walk_modules(BOILERDAQ / "experiments", BOILERDAQ))

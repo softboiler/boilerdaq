d0fffbdf947cf8f519681bb16347ee91677957e0
blakeNaccarato <blake.naccarato@gmail.com>
2023-08-30 16:12:37 -0700

    Move configs into data folder

--- src/boilerdaq/models/paths.py
+++ src/boilerdaq/models/paths.py
@@ -2,7 +2,7 @@
 
 from boilercore.models import CreatePathsModel
 from boilercore.paths import get_package_dir
-from pydantic import DirectoryPath
+from pydantic import DirectoryPath, FilePath
 
 import boilerdaq
 from boilerdaq.models import CWD
@@ -19,6 +19,18 @@ class Paths(CreatePathsModel):
     # ! Data
     data: DirectoryPath = project / "data"
     # * Git-Tracked Inputs
+    # ! Plotting config
+    plot_config: DirectoryPath = data / "plotting"
+    mpl_base: FilePath = plot_config / "base.mplstyle"
+    mpl_hide_title: FilePath = plot_config / "hide_title.mplstyle"
+    # ! Scripts
+    scripts: DirectoryPath = data / "scripts"
+    # ? Files
+    zotero: FilePath = scripts / "zotero.lua"
+    filt: FilePath = scripts / "filt.py"
+    csl: FilePath = scripts / "international-journal-of-heat-and-mass-transfer.csl"
+    template: FilePath = scripts / "template.dotx"
+
     # * DVC-Tracked Inputs
     # * Local Inputs
     config: DirectoryPath = data / "config"

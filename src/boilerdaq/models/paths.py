d6403ca4eb3886c24b5f7a3eee1bff00f3c5be0c
blakeNaccarato <blake.naccarato@gmail.com>
2023-10-24 15:10:47 -0700

    Migrate report generation to boilercore

--- src/boilerdaq/models/paths.py
+++ src/boilerdaq/models/paths.py
@@ -14,43 +14,30 @@ class Paths(CreatePathsModel):
     """Paths associated with this project."""
 
     # * Roots
-    # ! Project
     project: DirectoryPath = PROJECT_PATH
+    data: DirectoryPath = project / "data"
+
+    # * Local results
+    benchmarks: DirectoryPath = data / "benchmarks"
+    results: DirectoryPath = data / "results"
+    notes: DirectoryPath = data / "notes"
+
+    # * Git-tracked inputs
     # ! Package
     package: DirectoryPath = get_package_dir(boilerdaq)
     stages: dict[str, FilePath] = map_stages(package / "stages", package)
-    # ! Data
-    data: DirectoryPath = project / "data"
-
-    # * Git-Tracked Inputs
-    # ! Plotting config
-    plot_config: DirectoryPath = data / "plotting"
-    mpl_base: FilePath = plot_config / "base.mplstyle"
-    mpl_hide_title: FilePath = plot_config / "hide_title.mplstyle"
-    # ! Scripts
-    scripts: DirectoryPath = data / "scripts"
-    # ? Files
-    zotero: FilePath = scripts / "zotero.lua"
-    csl: FilePath = scripts / "international-journal-of-heat-and-mass-transfer.csl"
-    template: FilePath = scripts / "template.dotx"
-
-    # * DVC imports from boilercore
-    # ! Model Fit Function
-    model: Path = data / "model.dillpickle"
-
-    # * DVC-Tracked Inputs
-
-    # * Local Inputs
+    # ! Config
+    # Careful, "Config" is a special member of BaseClass
     config: DirectoryPath = data / "config"
     sensors_path = config / "1_sensors.csv"
     scaled_params_path = config / "2_scaled_params.csv"
     flux_params_path = config / "3_flux_params.csv"
     extrap_params_path = config / "4_extrap_params.csv"
     power_supplies_path = config / "0_power_supplies.csv"
+    # ! Plotting
+    plot_config: DirectoryPath = data / "plotting"
+    mpl_base: FilePath = plot_config / "base.mplstyle"
+    mpl_hide_title: FilePath = plot_config / "hide_title.mplstyle"
 
-    # * Local Results
-    benchmarks: DirectoryPath = data / "benchmarks"
-    results: DirectoryPath = data / "results"
-    notes: DirectoryPath = data / "notes"
-
-    # * DVC-Tracked Results
+    # * DVC-tracked imports
+    model: Path = data / "model.dillpickle"

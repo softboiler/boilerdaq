98b5a438c98db048c489a5a3d7fe2fadba11c1f4
blakeNaccarato <blake.naccarato@gmail.com>
2024-01-16 13:47:16 -0800

    Update boilercore pinned commit

--- .tools/scripts/Update-Boilercore.ps1
+++ .tools/scripts/Update-Boilercore.ps1
@@ -4,7 +4,7 @@ Update boilercore to the latest commit pin.
 
 git submodule update --init --remote --merge submodules/boilercore
 git add --all
-git commit -m "Update boilercore pinned commit"
+git commit --no-verify -m "Update boilercore pinned commit"
 git submodule deinit --force submodules/boilercore
 git add --all
-git commit --amend --no-edit
+git commit --no-verify --amend --no-edit

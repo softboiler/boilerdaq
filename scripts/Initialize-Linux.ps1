2a4c390fff11b7a7b6f5245856440301489c9194
blakeNaccarato <blake.naccarato@gmail.com>
2024-06-17 16:26:43 -0700

    Fix script

--- scripts/Initialize-Linux.ps1
+++ scripts/Initialize-Linux.ps1
@@ -6,8 +6,8 @@ if ($IsLinux) {
     # ! https://askubuntu.com/a/1460390
     # ! https://github.com/mccdaq/uldaq
     sudo apt install 'libegl1-mesa-dev' 'libxkbcommon0' 'libxcb-cursor0' 'libxcb-xinerama0' 'libqt5x11extras5' 'gcc' 'g++' 'make' 'bzip2' 'gzip' 'libusb-1.0-0-dev'
-    curl -L -O 'https://github.com/mccdaq/uldaq/releases/download/v1.2.1/libuldaq-1.2.1.tar.bz2'
 }
+curl -L -O 'https://github.com/mccdaq/uldaq/releases/download/v1.2.1/libuldaq-1.2.1.tar.bz2'
 $Archive = 'libuldaq-1.2.1.tar.bz2'
 tar -xvjf $Archive
 Remove-Item $Archive

85975388df30467422bb676d6db0f3e98601e5fe
blakeNaccarato <blake.naccarato@gmail.com>
2024-07-19 11:21:00 -0700

    Revert to default fit from `boilercore`

--- src/boilerdaq/models/params.py
+++ src/boilerdaq/models/params.py
@@ -16,15 +16,8 @@ PARAMS_FILE = get_params_file()
 class Params(SynchronizedPathsYamlModel):
     """Project parameters."""
 
-    fit: Fit = Field(
-        default=Fit(
-            free_params=["T_s", "q_s", "h_a"],
-            fixed_params=["h_w", "r", "T_infa", "T_infw", "x_s", "x_wa", "k"],
-        ),
-        description="Parameters for model fit.",
-    )
+    fit: Fit = Field(default_factory=Fit, description="Parameters for model fit.")
     geometry: Geometry = Field(default_factory=Geometry, description="Geometry.")
-
     paths: Paths = Field(default_factory=Paths)
     """Project paths."""
 
